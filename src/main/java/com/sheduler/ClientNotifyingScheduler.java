ba9bd401d05cd62d20d86e057cc668b589e1072b
SemenNechypurenko <semen.nechypurenko.gmail.com>
2022-04-18 15:52:26 +0200

    19 feat Add date and float value formatting

--- src/main/java/com/sheduler/ClientNotifyingScheduler.java
+++ src/main/java/com/sheduler/ClientNotifyingScheduler.java
@@ -7,14 +7,14 @@ import com.service.EmailService;
 import com.service.SubscriptionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
-import java.util.Date;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Configuration
@@ -25,12 +25,14 @@ public class ClientNotifyingScheduler {
     private final CoinService coinService;
     private final SubscriptionService subscriptionService;
     private final EmailService emailService;
+    private final DecimalFormat decimalFormat = new DecimalFormat("0.00");
 
     // subscription check occurs every 30 seconds
     @Scheduled(cron = "*/30 * * * * *")
     public void checkSubscriptions() {
         // all subscriptions
         Set<Subscription> subscriptions = new HashSet<>(subscriptionService.list());
+        decimalFormat.setRoundingMode(RoundingMode.DOWN);
         for (var subscription: subscriptions) {
             // get rate time frame
             List<Coin> coinsPerPeriodOfTime = coinService.list(subscription.getDateOfLastEmail(), new Date())
@@ -41,8 +43,7 @@ public class ClientNotifyingScheduler {
                 Coin lastCoin = coinsPerPeriodOfTime.get(coinsPerPeriodOfTime.size() - 1);
                 double delta;
                 String message = "Dear %s You are subscribed on crypto %s change, " +
-                        "and the rate currently have %s on %s percents for the period since " +
-                        "%s till %s.";
+                        "and the rate currently have %s on %s percents since %s .";
                 if ((lastCoin.getPrice() - firstCoin.getPrice())/firstCoin.getPrice() * 100
                         >= subscription.getDelta()) {
                     log.info("{} rate increased on {} percents", subscription.getCurrency(),
@@ -51,7 +52,8 @@ public class ClientNotifyingScheduler {
                     delta = (lastCoin.getPrice() - firstCoin.getPrice()) / firstCoin.getPrice() * 100;
 
                     message = String.format(message, subscription.getUser().getName(), subscription.getCurrency(),
-                            "increased", delta, subscription.getDateOfLastEmail(), new Date());
+                            "increased", decimalFormat.format(delta),
+                            DateUtils.truncate(subscription.getDateOfLastEmail(), Calendar.MINUTE));
 
                     emailService.sendEmail(subscription.getUser().getEmail(),
                             "Money rate has been changed", message);
@@ -60,13 +62,14 @@ public class ClientNotifyingScheduler {
                 }
                 if ((firstCoin.getPrice() - lastCoin.getPrice())/firstCoin.getPrice() * 100
                         >= subscription.getDelta()) {
-                    log.info("{} rate reduced on {} percents", subscription.getCurrency(),
-                            (firstCoin.getPrice() - lastCoin.getPrice())/firstCoin.getPrice() * 100);
 
-                    delta = (firstCoin.getPrice() - lastCoin.getPrice()) / firstCoin.getPrice() * 100;
+                    delta = (firstCoin.getPrice() - lastCoin.getPrice())/firstCoin.getPrice() * 100;
+
+                    log.info("{} rate reduced on {} percents", subscription.getCurrency(), decimalFormat.format(delta));
 
                     message = String.format(message, subscription.getUser().getName(), subscription.getCurrency(),
-                            "reduced", delta, subscription.getDateOfLastEmail(), new Date());
+                            "reduced", decimalFormat.format(delta),
+                            DateUtils.truncate(subscription.getDateOfLastEmail(), Calendar.MINUTE));
 
                     emailService.sendEmail(subscription.getUser().getEmail(), "Money rate has been changed",
                             message);
